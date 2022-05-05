51e75b785a6d19b426538e4a9d02834cdaa6b805
hherreratejos <hherreratejos98@gmail.com>
2022-05-04 22:20:41 -0700

    recursion added

--- W3D2/Memory_puzzle_group#6/Game.rb
+++ W3D2/Memory_puzzle_group#6/Game.rb
@@ -18,14 +18,14 @@ class Game
         count = 0
         while !@board.won?
             @board.render
-            pos1 = @player.get_position  # this is for human player
-            # pos1 = @ai.get_first_position(@board) #this is for ai
+            # pos1 = @player.get_position  # this is for human player
+            pos1 = @ai.get_first_position(@board) #this is for ai
             @board.grid[pos1[0]][pos1[1]].reveal
             @board.render
-            pos2 = @player.get_position # this is for human player
-            # pos2 = @ai.get_second_position(@board, pos1) #this is for ai
+            # pos2 = @player.get_position # this is for human player
+            pos2 = @ai.get_second_position(@board, pos1) #this is for ai
             @board.grid[pos2[0]][pos2[1]].reveal
-            # @ai.receive_match(@board, pos1, pos2) #this is for ai
+            @ai.receive_match(@board, pos1, pos2) #this is for ai
             @board.render
             self.hide?(@board.grid[pos1[0]][pos1[1]], @board.grid[pos2[0]][pos2[1]])
             count += 1
